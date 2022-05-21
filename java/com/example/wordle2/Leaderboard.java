2d05608d64609a03a8c81e03e843e6bcd22194f4
Richard J. Field <chrfield@bmchsd.org>
2022-05-21 13:24:16 -0400

    feat: added leaderboard

--- java/com/example/wordle2/Leaderboard.java
+++ java/com/example/wordle2/Leaderboard.java
@@ -9,6 +9,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class Leaderboard extends AppCompatActivity {
     TextView tv1;
@@ -42,7 +47,47 @@ public class Leaderboard extends AppCompatActivity {
                 switchActivities2();
             }
         });
+        getLeaderboard();
 
+    }
+    public void getLeaderboard() {
+        try {
+            //URL url = new URL("http://localhost:8080/leaderboard");
+            URL url = new URL("https://kordle-svr-5bu27xjxoq-ue.a.run.app/leaderboard");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/text");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+            String userColText="";
+            String userTriesText="";
+            String userTimeText="";
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println();
+                String [] parts =output.split(",");
+                System.out.println(parts[0]);
+                System.out.println(parts[1]);
+                System.out.println(parts[2]);
+                userColText=userColText+parts[0]+"\n\n";
+                userTriesText=userTriesText+parts[1]+"\n\n";
+                userTimeText=userTimeText+parts[2]+"\n\n";
+
+            }
+            conn.disconnect();
+            tv4.setText(userColText);
+            tv5.setText(userTriesText);
+            tv6.setText(userTimeText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     private void switchActivities2(){
         Intent switchActivityIntent=new Intent(this,mainScreen.class);
